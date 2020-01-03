64a05c38f79d31c31b4969e3ac2cbe547bcf1225
Rona27 <rona.1125@students.amikom.ac.id>
2020-01-03 18:04:47 +0700

    Hello word

--- app/src/main/java/com/example/barangqu/MainActivity.java
+++ app/src/main/java/com/example/barangqu/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.barangqu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView coba;
+    Button bCoba;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
