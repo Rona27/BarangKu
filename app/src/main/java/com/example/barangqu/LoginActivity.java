0f8ce5dc795a005d9402e62c2285a8a07b797b54
Rona27 <rona.1125@students.amikom.ac.id>
2020-01-04 23:49:31 +0700

    Hello word

--- app/src/main/java/com/example/barangqu/LoginActivity.java
+++ app/src/main/java/com/example/barangqu/LoginActivity.java
@@ -1,35 +1,107 @@
 package com.example.barangqu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Person;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.barangqu.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
-
-    Button btnLogin, btnDaftar;
+     private EditText edtEmail, edtPassword;
+     private FirebaseAuth logAuth;
+     private ProgressBar logProgres;
+     private Button btnLogin, btnDaftar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //get firebase auth instance
+        logAuth = FirebaseAuth.getInstance();
+
+        if (logAuth.getCurrentUser() != null) {
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            finish();
+        }
         setContentView(R.layout.activity_login);
 
-        btnDaftar = findViewById(R.id.btn_daftar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        edtEmail = (EditText) findViewById(R.id.edt_email);
+        edtPassword = (EditText) findViewById(R.id.edt_password);
+        logProgres =(ProgressBar) findViewById(R.id.progressBar);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        btnDaftar = (Button) findViewById(R.id.btn_daftar);
+
+        logAuth = FirebaseAuth.getInstance();
+
+        //intent ke daftar
 
         btnDaftar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(intent);
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
                 finish();
             }
         });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 String email = edtEmail.getText().toString();
+                final String password = edtPassword.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Email tidak boleh kosong !", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Password tidak boeh kosong !", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                logProgres.setVisibility(View.VISIBLE);
+
+                //autentikasi user
+                logAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                logProgres.setVisibility(View.GONE);
+                                if (task.isSuccessful()){
+                                    Intent logIntent = new Intent(LoginActivity.this, MainActivity.class);
+                                    startActivity(logIntent);
+                                    finish();
+                                }
+                                else {
+                                    if (password.length() < 6){
+                                        edtPassword.setError(getString(R.string.minimum_password));
+                                    } else {
+                                        Toast.makeText(LoginActivity.this, getString(R.string.auth_gagal), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }
+                        });
+            }
+        });
+
+
     }
 
 }
